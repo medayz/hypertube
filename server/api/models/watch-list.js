5ac45c60e4bd59662871776b67ad7d1b585fa45c
Ayoub AMINE <programmer010011@gmail.com>
2020-01-04 23:31:43 +0100

    watch list schema

--- server/api/models/watch-list.js
+++ server/api/models/watch-list.js
@@ -1,17 +1,9 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 const watchListSchema = new Schema({
   user: { type: Schema.Types.ObjectId, required: true },
-  movies: [
-    {
-      id: { type: Number, required: true },
-      provider: {
-        enum: ["YTS", "POPCORN"],
-        required: true
-      }
-    }
-  ],
+  movies: [{ type: String, required: true }],
   createdAt: { type: Date, default: Date.now }
 });
 
-module.exports.watchListSchema = model("watchlist", watchListSchema);
+module.exports.watchListSchema = model('watchlist', watchListSchema);
