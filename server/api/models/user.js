fc215c1718f4f1ec5612d636009b886d20ba6f9b
Ayoub AMINE <programmer010011@gmail.com>
2020-01-04 23:30:07 +0100

    Updated user schema

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -1,11 +1,32 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  fullName: { type: String, required: true },
+  username: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String, select: false, required: true },
   createdAt: { type: Date, default: Date.now }
 });
 
-module.exports.User = model("User", userSchema);
+userSchema.pre('save', function(next) {
+  // Only hash the password if it has been modified (or is new)
+  if (!this.isModified('password')) return next();
+
+  // Hash the password using our new salt
+  bcrypt.hash(this.password, 10, (err, hash) => {
+    if (err) return next(err);
+
+    // Override the cleartext password with the hashed one
+    this.password = hash;
+    next();
+  });
+});
+
+userSchema.methods.isValidPassword = async function(password) {
+  const compare = await bcrypt.compare(password, this.password);
+
+  return compare;
+};
+
+module.exports = model('User', userSchema);
