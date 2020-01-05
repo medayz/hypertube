02ecc1c7edbae4be3330438ff4af3d21dac7ed01
bahlaouiyoussef <byoussef773@gmail.com>
2020-01-05 17:05:41 +0100

    Add update user route

--- server/api/controllers/users.js
+++ server/api/controllers/users.js
@@ -2,20 +2,22 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
-const passport = require('../middlewares/auth');
+const passport = require('../middlewares/passport');
 
 exports.create = async (req, res, next) => {
   const user = new User(req.body);
 
   try {
-    const result = await User.findOne({
-      $or: [{ username: req.body.username }, { email: req.body.email }]
-    });
+    const usernameExists = await User.usernameExists(req.body.username);
 
-    if (result) {
-      const key = result.username === req.body.username ? 'username' : 'email';
+    if (usernameExists) {
+      return res.status(403).send({ message: 'username is already exists' });
+    }
 
-      return res.status(403).send({ message: `${key} is already exists` });
+    const emailExists = await User.emailExists(req.body.email);
+
+    if (emailExists) {
+      return res.status(403).send({ message: 'email is already exists' });
     }
 
     const newUser = await user.save();
@@ -35,8 +37,6 @@ exports.create = async (req, res, next) => {
 
 exports.login = (req, res, next) => {
   passport.authenticate('login', (err, user, info) => {
-    console.log(err, user, info);
-
     if (err) return next(err);
 
     if (info) return res.status(401).send(info);
@@ -52,6 +52,46 @@ exports.me = async (req, res, next) => {
   try {
     const user = await User.findOne({ _id: req.user._id });
     res.status(200).send(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.update = async (req, res, next) => {
+  try {
+    if (req.body.username) {
+      const usernameExists = await User.usernameExists(req.body.username);
+
+      if (usernameExists) {
+        return res.status(403).send({ message: 'username is already exists' });
+      }
+    }
+
+    if (req.body.email) {
+      const emailExists = await User.emailExists(req.body.email);
+
+      if (emailExists) {
+        return res.status(403).send({ message: 'email is already exists' });
+      }
+    }
+
+    let user = await User.findOne({ _id: req.user._id });
+
+    // Check email is updated
+    if (req.body.email && req.body.email !== user.email) {
+      user.emailActivated = false;
+    }
+
+    // Update user object with given values
+    for (const key in req.body) {
+      const value = req.body[key];
+
+      user[key] = value;
+    }
+
+    user = await user.save();
+
+    res.status(200).send({ message: 'User updated', user });
   } catch (err) {
     next(err);
   }
