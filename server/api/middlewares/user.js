ddf1dfb146b91d68fdb08d76167ebfd1ae53155f
bahlaouiyoussef <byoussef773@gmail.com>
2020-01-05 17:07:29 +0100

    Add prettyError util function

--- server/api/middlewares/user.js
+++ server/api/middlewares/user.js
@@ -1,21 +1,29 @@
-const { createUserValidator, loginUserValidator } = require('../validators/user');
+const userSchema = require('../validators/user');
 
 exports.createValidator = (req, res, next) => {
-  const { error } = createUserValidator.validate(req.body);
+  const { error } = userSchema.createUserValidator.validate(req.body);
 
   if (!error) return next();
 
-  res.status(400).send({
-    error: error.details.map(detail => detail.message)
-  });
+  res.status(400).send({ error: prettyErrors(error) });
 };
 
 exports.loginValidator = (req, res, next) => {
-  const { error } = loginUserValidator.validate(req.body);
+  const { error } = userSchema.loginUserValidator.validate(req.body);
 
   if (!error) return next();
 
-  res.status(400).send({
-    error: error.details.map(detail => detail.message)
-  });
+  res.status(400).send({ error: prettyErrors(error) });
 };
+
+exports.updateValidator = (req, res, next) => {
+  const { error } = userSchema.updateUserValidator.validate(req.body);
+
+  if (!error) return next();
+
+  res.status(400).send({ error: prettyErrors(error) });
+};
+
+function prettyErrors(error) {
+  return error.details.map(detail => detail.message);
+}
