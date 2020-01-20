de28cb08995031e7eac356ca985b3bdb5c8e4805
bahlaouiyoussef <byoussef773@gmail.com>
2020-01-20 19:51:21 +0100

    Updated

--- server/api/models/watch-list.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const watchListSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, required: true },
-  movies: [{ type: String, required: true }],
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports.watchListSchema = model('watchlist', watchListSchema);
--- server/api/utils/generateToken.js
+++
@@ -1,5 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-module.exports = (payload) => {
-  return jwt.sign(payload, process.env.JWT_KEY);
-};
