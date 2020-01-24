80102fc8832bed4124ed51321866bb0df2d87409
bahlaouiyoussef <byoussef773@gmail.com>
2020-01-24 15:54:38 +0100

    Add email utils to index

--- server/api/utils/index.js
+++ server/api/utils/index.js
@@ -1,8 +1,10 @@
 const { generateToken, verfiyToken } = require('./jwt');
+const { sendEmail } = require('./email');
 const prettyError = require('./prettyerror');
 
 module.exports = {
   generateToken,
   verfiyToken,
+  sendEmail,
   prettyError
 };
