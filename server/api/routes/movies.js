27f2f94892f290830ccd7f8945c973cee501c084
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2020-02-20 00:27:54 +0100

    Add isAuth middleware to global use

--- server/api/routes/movies.js
+++ server/api/routes/movies.js
@@ -3,9 +3,10 @@ const { isAuth } = require('../middlewares/auth');
 const moviesController = require('../controllers/movies');
 const movieValidator = require('../middlewares/movie');
 
+router.use(isAuth);
+
 router.get(
   '/',
-  isAuth,
   movieValidator.getMoviesValidator,
   movieValidator.cacheMovies,
   moviesController.getMovies
@@ -13,7 +14,6 @@ router.get(
 
 router.get(
   '/search',
-  isAuth,
   movieValidator.searchValidator,
   movieValidator.cacheSearch,
   moviesController.search
@@ -21,7 +21,6 @@ router.get(
 
 router.get(
   '/:imdbid',
-  isAuth,
   movieValidator.getMovieValidator,
   movieValidator.cacheMovie,
   moviesController.getMovie
@@ -29,28 +28,24 @@ router.get(
 
 router.get(
   '/comments/:imdbid',
-  isAuth,
   movieValidator.getCommentsValidator,
   moviesController.getComments
 );
 
 router.post(
   '/comments/:imdbid',
-  isAuth,
   movieValidator.addCommentValidator,
   moviesController.addComment
 );
 
 router.patch(
   '/comments/:imdbid/:id',
-  isAuth,
   movieValidator.updateCommentValidator,
   moviesController.updateComment
 );
 
 router.delete(
   '/comments/:imdbid/:id',
-  isAuth,
   movieValidator.deleteCommentValidator,
   moviesController.deleteComment
 );
