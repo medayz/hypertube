7203f5f670707109b3c5bb16015cb2aa04afea1f
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2020-02-25 17:10:27 +0100

    Delete unused model

--- server/api/models/cache.js
+++
@@ -1,7 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const movieSchema = new Schema({
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports.Cache = model("Cache", movieSchema);
