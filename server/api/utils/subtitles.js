be1be83dcdb1c2285fc78b9c26e112f5155abc84
medayz <medayoubz4997@gmail.com>
2020-03-04 09:57:25 +0100

    return empty array when no subtitles are found

--- server/api/utils/subtitles.js
+++ server/api/utils/subtitles.js
@@ -2,20 +2,24 @@ const getSubtitles = require('yifysubtitles');
 const fs = require('fs');
 
 exports.get = async imdbid => {
-  fs.mkdirSync(`${process.env.MOVIES_PATH}/${imdbid}`, {
-    recursive: true
-  });
+  try {
+    fs.mkdirSync(`${process.env.MOVIES_PATH}/${imdbid}`, {
+      recursive: true
+    });
 
-  let subtitles = await getSubtitles(imdbid, {
-    path: `${process.env.MOVIES_PATH}/${imdbid}`,
-    langs: ['ar', 'fr', 'en', 'es']
-  });
+    let subtitles = await getSubtitles(imdbid, {
+      path: `${process.env.MOVIES_PATH}/${imdbid}`,
+      langs: ['ar', 'fr', 'en', 'es']
+    });
 
-  subtitles = subtitles.map(item => ({
-    lang: item.lang,
-    langShort: item.langShort,
-    fileName: item.fileName
-  }));
+    subtitles = subtitles.map(item => ({
+      lang: item.lang,
+      langShort: item.langShort,
+      fileName: item.fileName
+    }));
 
-  return subtitles;
+    return subtitles;
+  } catch (err) {
+    return [];
+  }
 };
